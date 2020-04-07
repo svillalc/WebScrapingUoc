263f16bc9529c8daac60a49a358e4bc7ff9810f6
svillalc <svillalc@users.noreply.github.com>
2020-04-07 19:05:28 +0200

    Codigo comentado

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -21,7 +21,7 @@ headers = {
 req = requests.get("https://www.idealista.com/alquiler-viviendas/alicante-alacant-alicante/", headers=headers)
 soup = BeautifulSoup(req.text, "html.parser")
 
-# Numero de viviendas   SOLO SE PUEDE LANZAR UNA VEZ CADA 10 MIN APROX
+# Numero de viviendas
 required0 = soup.find_all("h1")
 
 nviviendas = []
@@ -31,20 +31,17 @@ for i in required0:
     numero_viviendas = int(nviviendas[0].split()[0].replace(".", ""))
 print(numero_viviendas)
 
-# Numero de paginas
+# Numero de paginas a partir del numero total de viviendas
+# Idealista muestra 30 viviendas por pagina
 numero_paginas = math.ceil(numero_viviendas / 30)
 
-# restamos 1 porque la ultima pagina la recorreremos a parte <- Esto creo que no hace falta
-
-# Numero de elementos pagina final
-elementos_pagina_final = numero_viviendas % 30
-
-# Recogemos la informacion de las N-1 primeras paginas   CASI SEGURO QUE TENDREMOS QUE METER UN DELAY para que no sature
-
+# Inicializamos las columnas del dataset
 precios = []
 habitaciones = []
 metros = []
 
+# Recogemos la informacion de cada pagina.
+# Introducimos un sleep para no saturar el servidor
 for i in range(numero_paginas):
     req2 = requests.get(
         'https://www.idealista.com/alquiler-viviendas/alicante-alacant-alicante/pagina-' + str(i + 1) + '.htm',
@@ -53,6 +50,7 @@ for i in range(numero_paginas):
     soup2 = BeautifulSoup(req2.text, "html.parser")
     anuncios2 = soup2.find_all("div", "item-info-container")
 
+    # Listas temporales
     precios2 = []
     habitaciones2 = []
     metros2 = []
@@ -74,38 +72,25 @@ for i in range(numero_paginas):
         habitaciones2.append(numhabs)
         metros2.append(nummetros)
 
-    # precios2 = [anuncio2.find("span", "item-price h2-simulated").text for anuncio2 in anuncios2]
-    # habitaciones2 = [anuncio2.find_all("span", "item-detail")[0].text for anuncio2 in anuncios2]
-    # metros2 = [anuncio2.find_all("span", "item-detail")[1].text for anuncio2 in anuncios2]
-    # precios_sin_unidad2 = [re.findall(r"^\d+\.*\d*", precio)[0] for precio in precios2]
-
+    # Anyadimos las listas temporales a nuestras listas globales
     precios = precios + precios2
     habitaciones = habitaciones + habitaciones2
     metros = metros + metros2
+
+    # Introducimos el sleep
     time.sleep(10)
 
-#################################################################################################
-# Hasta aqui anyade las N-1 paginas y funciona, falta anyadir la ultima pagina
-###################################################################################################
 
-
-# corregimos los metros que no vengan como tal sustituyendo como N/A
-# metros_corregido = []
-# for metro in metros:
-# if ("m²" in metro):
-# metros_corregido = metros_corregido + [metro.split()[0]]
-# else:
-# metros_corregido = metros_corregido +['N/A']
-
-
+# Formamos el dataset
 data = {"Precio (Euros/Mes)": precios,
         "Habitaciones": habitaciones,
         "Tamanyo (m2)": metros}
 df = pd.DataFrame(data)
 
+# Le damos formato a las columnas y calculamos una nueva (precio por metro cuadrado)
 df["Precio (Euros/Mes)"] = list(map(lambda x: x.replace(".", "").split("€")[0], df["Precio (Euros/Mes)"]))
 df["Tamanyo (m2)"] = list(map(lambda x: x.split()[0], df["Tamanyo (m2)"]))
 df["Euros/m2"] = round(df["Precio (Euros/Mes)"].astype(int) / df["Tamanyo (m2)"].astype(int), 2)
-print(df)
 
-# df.to_csv("dataset.csv")
+# Exportamos el dataset
+df.to_csv("dataset.csv")
